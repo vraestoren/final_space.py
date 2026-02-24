699b1af118ce50e151f3f7e5d3512522be6e020d
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 13:32:39 +0300

    Update final_space.py

--- src/final_space.py
+++ src/final_space.py
@@ -1,49 +1,37 @@
-from requests import get
+from requests import Session
 
 class FinalSpace:
 	def __init__(self) -> None:
 		self.api = "https://finalspaceapi.com/api/v0"
-		self.headers = {
+		self.session = Session()
+		self.session.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
 			"accept": "application/json"
 		}
 	
 	def get_endpoints(self) -> dict:
-		return get(
-			f"{self.api}/",
-			headers=self.headers).json()
+		return self.session.get(self.api).json()
 	
 	def get_character(self, character_id: int) -> dict:
-		return get(
-			f"{self.api}/character/{character_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/character/{character_id}").json()
 			
 	def get_all_characters(self) -> dict:
-		return get(
-			f"{self.api}/character",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/character").json()
 	
 	def get_episode(self, episode_id: int) -> dict:
-		return get(
-			f"{self.api}/episode/{episode_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/episode/{episode_id}").json()
 			
 	def get_all_episodes(self) -> dict:
-		return get(
-			f"{self.api}/episode",
-			headers=self.headers).json()
-	
+		return self.session.get(f"{self.api}/episode").json()
+
 	def get_location(self, location_id: int) -> dict:
-		return get(
-			f"{self.api}/locations/{location_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/locations/{location_id}").json()
 			
 	def get_all_locations(self) -> dict:
-		return get(
-			f"{self.api}/locations",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/locations").json()
 	
 	def get_all_quotes(self) -> dict:
-		return get(
-			f"{self.api}/quote",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/quote").json()
